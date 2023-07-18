36b20fe386f698bd006172e7eb02ccb7cc3577fd
Camille L <camille0756@gmail.com>
2023-07-18 18:13:40 +0200

    Filters and fetch categories

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,26 +1,37 @@
-const init = async function() {
-    const response = await fetch('http://localhost:5678/api/works');
-    const works = await response.json();
-    console.log(works);
 
-    const gallery = document.querySelector(".gallery");
-    gallery.innerHTML = works.toString();
-    
-    works.forEach(work => {
-        console.log(work);
-    });
-    
-        let img = document.createElement('img');
-        img.src = "http://localhost:5678/images/abajour-tahina1651286843956.png";
-        document.getElementById('portfolio').appendChild(img);
-        down.innerHTML = "Abajour Tahina";
+const getCategories = async function() {
+    const response = await fetch('http://localhost:5678/api/categories');
+    const categories = await response.json();
+    console.log(categories);
+
+    categories.forEach(category => {
+        console.log(category);
+    })
 }
 
+const getProjects = async function() {
+    const response = await fetch('http://localhost:5678/api/works');
+    const projects = await response.json();
+    console.log(projects);
 
-init();
+    const gallery = document.querySelector('.gallery');
 
-fetch('http://localhost:5678/api/categories')
-    .then(res => res.json())
-    .then (data => console.log(data))
+    gallery.innerHTML = '';
+  
+    projects.forEach(project => {
+        const figure = document.createElement('figure');
+        const img = document.createElement('img');
+        img.src = project.imageUrl;
+        img.alt = project.title;
 
+        const figcaption = document.createElement('figcaption');
+        figcaption.innerHTML = project.title;
 
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+    });  
+}
+
+getCategories();
+getProjects();
