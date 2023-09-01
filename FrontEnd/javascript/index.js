473090ff04141caacdc6d1d4da22eb89ddbac96f
Camille L <camille0756@gmail.com>
2023-09-01 15:01:54 +0200

    icon move

--- FrontEnd/javascript/index.js
+++ FrontEnd/javascript/index.js
@@ -94,9 +94,21 @@ function displayWorks(projects) {
         figcaption.innerHTML = project.title;
         figcaption.innerHTML = "éditer";
 
-        const icon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        icon.classList.add('svg-icon');
-        icon.innerHTML = `
+        const iconMove = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        iconMove.classList.add('svg-move');
+        iconMove.innerHTML = `
+            <g id="Move">
+                <rect id="Rectangle 20" width="17" height="17" rx="2" fill="black"/>
+                <path id="arrows-up-down-left-right" d="M9.05089 3.20363C8.77938 2.93212 8.33845 2.93212 8.06694 3.20363L6.6768 4.59377C6.40529 4.86528 6.40529 5.30621 6.6768 5.57772C6.94831 5.84924 7.38925 5.84924 7.66076 5.57772L7.86493 5.37355V7.86493H5.37355L5.57772 7.66076C5.84924 7.38925 5.84924 6.94831 5.57772 6.6768C5.30621 6.40529 4.86528 6.40529 4.59377 6.6768L3.20363 8.06694C2.93212 8.33845 2.93212 8.77938 3.20363 9.05089L4.59377 10.441C4.86528 10.7125 5.30621 10.7125 5.57772 10.441C5.84924 10.1695 5.84924 9.72858 5.57772 9.45707L5.37355 9.2529H7.86493V11.7465L7.66076 11.5423C7.38925 11.2708 6.94831 11.2708 6.6768 11.5423C6.40529 11.8138 6.40529 12.2547 6.6768 12.5262L8.06694 13.9164C8.33845 14.1879 8.77938 14.1879 9.05089 13.9164L10.441 12.5262C10.7125 12.2547 10.7125 11.8138 10.441 11.5423C10.1695 11.2708 9.72858 11.2708 9.45707 11.5423L9.2529 11.7465V9.25507H11.7465L11.5423 9.45924C11.2708 9.73076 11.2708 10.1717 11.5423 10.4432C11.8138 10.7147 12.2547 10.7147 12.5262 10.4432L13.9164 9.05306C14.1879 8.78155 14.1879 8.34062 13.9164 8.06911L12.5262 6.67897C12.2547 6.40746 11.8138 6.40746 11.5423 6.67897C11.2708 6.95048 11.2708 7.39142 11.5423 7.66293L11.7465 7.8671H9.25507V5.37355L9.45924 5.57772C9.73076 5.84924 10.1717 5.84924 10.4432 5.57772C10.7147 5.30621 10.7147 4.86528 10.4432 4.59377L9.05306 3.20363H9.05089Z" fill="white"/>
+            </g>
+        `;
+
+        const moveBtn = document.createElement('button');
+        moveBtn.classList.add('btn-move');
+
+        const iconBin = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        iconBin.classList.add('svg-icon');
+        iconBin.innerHTML = `
           <g id="trash-can-solid">
             <path id="Vector" d="M2.71607 0.35558C2.82455 0.136607 3.04754 0 3.29063 0H5.70938C5.95246 0 6.17545 0.136607 6.28393 0.35558L6.42857 0.642857H8.35714C8.71272 0.642857 9 0.930134 9 1.28571C9 1.64129 8.71272 1.92857 8.35714 1.92857H0.642857C0.287277 1.92857 0 1.64129 0 1.28571C0 0.930134 0.287277 0.642857 0.642857 0.642857H2.57143L2.71607 0.35558ZM0.642857 2.57143H8.35714V9C8.35714 9.70915 7.78058 10.2857 7.07143 10.2857H1.92857C1.21942 10.2857 0.642857 9.70915 0.642857 9V2.57143ZM2.57143 3.85714C2.39464 3.85714 2.25 4.00179 2.25 4.17857V8.67857C2.25 8.85536 2.39464 9 2.57143 9C2.74821 9 2.89286 8.85536 2.89286 8.67857V4.17857C2.89286 4.00179 2.74821 3.85714 2.57143 3.85714ZM4.5 3.85714C4.32321 3.85714 4.17857 4.00179 4.17857 4.17857V8.67857C4.17857 8.85536 4.32321 9 4.5 9C4.67679 9 4.82143 8.85536 4.82143 8.67857V4.17857C4.82143 4.00179 4.67679 3.85714 4.5 3.85714ZM6.42857 3.85714C6.25179 3.85714 6.10714 4.00179 6.10714 4.17857V8.67857C6.10714 8.85536 6.25179 9 6.42857 9C6.60536 9 6.75 8.85536 6.75 8.67857V4.17857C6.75 4.00179 6.60536 3.85714 6.42857 3.85714Z" fill="white"/>
           </g>
@@ -112,15 +124,17 @@ function displayWorks(projects) {
                 await getProjects();
                 return;
             }
-            console.log(trashBtn.id)
         });
 
         const figure = document.createElement('figure');
         figure.classList.add('figure-list');
+
         figure.appendChild(img);
         figure.appendChild(figcaption);
+        figure.appendChild(moveBtn);
+        moveBtn.appendChild(iconMove);
         figure.appendChild(trashBtn);
-        trashBtn.appendChild(icon);
+        trashBtn.appendChild(iconBin);
         modalContent.appendChild(figure);
     });
 }
@@ -157,13 +171,13 @@ function showModal2() {
                 const reader = new FileReader();
                 imgDw.style.visibility = 'visible';
                 document.querySelector('.add-photo').style.visibility = 'hidden';
+                document.querySelector('.img-span').style.visibility = 'hidden';
                 reader.readAsDataURL(file);
 
                 reader.addEventListener('load', function(){
                     imgDw.setAttribute('src', this.result);
                 });
             };
-
         });
     
         const categoryInput = templateAddWorkForm.getElementById('category-input');
